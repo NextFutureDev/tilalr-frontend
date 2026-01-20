99e3f515ddf99b4ade02edd80df0f903aec27882
engr-amanshah <nextfuture.dev@gmail.com>
2026-01-20 16:54:32 +0300

    uploaded

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "d1foa0aaimjyw4.cloudfront.net",
+      "admin.tilalr.com",
+    ],
+  },
+};
+
+export default nextConfig;
+
+
